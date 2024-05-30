3c75811586a67772fe89aeffb92cba3db4c7a557
Vivek <vivekalumootil@gmail.com>
2024-05-30 11:02:21 -0700

    updated top.v to have both digits as 1

--- project_4.srcs/sources_3/top.v
+++ project_4.srcs/sources_3/top.v
@@ -5,7 +5,7 @@ module top(clk,an,seg);
     output wire [6:0] seg;
     wire [3:0] display_num;
  
-    anode_cycle anode(.clk(clk), .i1(sec0), .i2(sec1), .led_output(display_num), .an(an));
+    anode_cycle anode(.clk(clk), .i1(4'b0001), .i2(4'b0001), .led_output(display_num), .an(an));
     convert_7seg convert(.num(display_num),.seg(seg));
 	
 endmodule
